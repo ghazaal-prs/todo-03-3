c0502e12a712fbe5d4f75ccd75b9ff447fc0d716
ghazaal <ghazaal.poursafar@gmail.com>
2023-08-30 14:37:53 +0430

    todo 03-3

---
+++ index.js
@@ -0,0 +1,20 @@
+function number(a = 0 , b = 0 , c = 0 , d = 0 , e = 0 ){
+
+    const plus = a + b + c + d + e
+    const average = plus / 5 ;
+    if (average < 12) console.log("mashroot");
+
+    return average
+
+
+}
+const num1 = +prompt("num1")
+    const num2 = +prompt("num2")
+    const num3 = +prompt("num3")
+    const num4 = +prompt("num4")
+    const num5 = +prompt("num5")
+
+    console.log(number(num1 , num2 , num3 , num4 , num5))
+
+  
+    
